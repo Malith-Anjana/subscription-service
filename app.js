3affec14c08365cedf87cff8fe8354439cb65569
Malith Anjana <malithanjana97@gmail.com>
2025-08-25 10:41:39 +0530

    create error middleware

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 import express from 'express';
+import cookieParser from 'cookie-parser'
 import {PORT} from "./config/env.js";
 
-import userRoouter from "./routes/user.routes.js";
+import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
+// allow handle json data that send in request
+app.use(express.json());
+
+// help to process form data send via html form in a simple format
+app.use(express.urlencoded({ extended: true }));
+
+// read cookies from incoming req
+app.use(cookieParser())
+
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/user', userRoouter);
+app.use('/api/v1/user', userRouter);
 app.use('/api/v1/subscription', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res)=>{
     res.send('Welcome to the Personal Projects');
