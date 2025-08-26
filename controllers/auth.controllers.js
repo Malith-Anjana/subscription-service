a9f332e3508522be07f60d567b1b6f2b814635a2
Malith Anjana <malithanjana97@gmail.com>
2025-08-26 11:21:42 +0530

    implement sign-up and sign-in logic

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -2,42 +2,88 @@ import mongoose from "mongoose";
 import User from "../models/user.models.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import {JWT_EXPIRES_IN, JWT_SECRET} from "../config/env.js";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
 export const signUp = async (req, res, next) => {
-    // Database operations are atomic. (ex: Insert either works completely or it doesn't)
-    const session = await mongoose.startSession();
-    session.startTransaction();
+  // Database operations are atomic. (ex: Insert either works completely or it doesn't)
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-    try{
+  try {
     const { name, email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-        const error = new Error('User already exists');
-        error.statusCode = 409;
-        throw error;
+      const error = new Error("User already exists");
+      error.statusCode = 409;
+      throw error;
     }
 
     // Hash Password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-        // if something went wrong when creating user, 'session' will identify that and trigger the catch block
-        const newUsers = await User.create([{name, email, password,hashedPassword}], {session});
-        const token = jwt.sign({userId : newUsers[0]._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+    // if something went wrong when creating user, 'session' will identify that and trigger the catch block
+    const newUsers = await User.create(
+      [{ name, email, password: hashedPassword }],
+      { session },
+    );
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
 
     await session.commitTransaction();
     await session.endSession();
 
-    res.status(201).json({success: true, message: 'User successfully created', data: {token, user: newUsers[0]}});
+    res.status(201).json({
+      success: true,
+      message: "User successfully created",
+      data: { token, user: newUsers[0] },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    next(error);
+  } finally {
+    // Always close session
+    await session.endSession();
+  }
+};
 
-    }catch(error){
-        await session.abortTransaction();
-        await session.endSession();
-        next(error);
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
     }
-}
-export const signIn = async (req, res, next) => {}
-export const signOut = async (req, res, next) => {}
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      const error = new Error("Invalid Password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User signed in successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signOut = async (req, res, next) => {};
