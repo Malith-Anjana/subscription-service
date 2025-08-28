f152ada32f3eb3d228b760d9d50f7a4d3ecc15ab
Malith Anjana <malithanjana97@gmail.com>
2025-08-28 17:40:55 +0530

    implement reminder workflow

--- config/env.js
+++ config/env.js
@@ -6,10 +6,15 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 export const {
   PORT,
+  SERVER_URL,
   NODE_ENV,
   DB_URI,
   JWT_EXPIRES_IN,
   JWT_SECRET,
   ARCJET_KEY,
   ARCJET_ENV,
+  QSTASH_CURRENT_SIGNING_KEY,
+  QSTASH_URL,
+  QSTASH_NEXT_SIGNING_KEY,
+  QSTASH_TOKEN,
 } = process.env;
